9fa991591ab22f6f4fa69e08f45fcc17d9d2fc74
190101101 <190101101@mail.ru>
2023-10-13 09:13:21 +0400

    02 typescript dating\06 running typescript code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello typescript')
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => {
+    console.log(response.data);
+})
