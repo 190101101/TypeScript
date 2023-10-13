8ac52a041315f0857cd6506f6ae82fb83718e447
190101101 <190101101@mail.ru>
2023-10-13 21:46:27 +0400

    02 ts dating\08 detecting errors using ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
+interface Todo {
+  id: Number;
+  title: String;
+  completed: Boolean;
+}
 
-axios.get(url).then(response => {
-    console.log(response.data);
-})
+const url = "https://jsonplaceholder.typicode.com/todos/4";
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    id: ${id}
+    title: ${title}
+    completed: ${completed}
+  `);
+});
