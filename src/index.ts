5e0393deb4fd8f3578d9d853c2402f4bc6efbc33
190101101 <190101101@mail.ru>
2023-10-14 02:51:25 +0400

    04 Annotations in act\18 for object literals

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,26 @@ let hasStatus:boolean = true;
 let nothing1:null = null;
 let nothing2:undefined = undefined;
 
+//built-in objects
 let now:Date = new Date();
+
+//array
+let colors:string[] = ['black', 'green', 'yellow', 'blue'];
+let numbers: number[] = [1,2,3,4]
+let results: boolean[] = [true, false, true]
+
+//classes
+
+class Car{
+
+}
+
+let car:Car = new Car();
+
+//object literal
+let point:{x:number; y:number} = {
+    x:10,
+    y:20
+}
+
+console.log(point);
