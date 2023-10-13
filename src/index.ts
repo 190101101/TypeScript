34212a87f23be7963c95bf5a16f5ccae1b22b7fa
190101101 <190101101@mail.ru>
2023-10-13 23:49:34 +0400

    03 system types\14 where are types used

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
-import axios from "axios";
+const today = new Date();
 
-interface Todo {
-  id: Number;
-  title: String;
-  completed: Boolean;
-}
+const person = {
+  age:21
+};
 
-const url = "https://jsonplaceholder.typicode.com/todos/4";
+class Color{}
 
-axios.get(url).then((response) => {
-  const todo = response.data as Todo;
+const red = new Color();
 
-  const id = todo.id;
-  const title = todo.title;
-  const completed = todo.completed;
-
-  logTodo(id, title, completed);
-});
-
-const logTodo = (id: Number, title: String, completed: Boolean) => {
-  console.log(`
-  id: ${id}
-  title: ${title}
-  completed: ${completed}
-  `);
-}
