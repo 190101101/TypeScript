6471f138b77577b2e158f559ed8056b9edd427b3
190101101 <190101101@mail.ru>
2023-10-14 09:12:43 +0400

    04 Annotations in act\23 lazy initialization

--- src/index.ts
+++ src/index.ts
@@ -38,3 +38,14 @@ logNumber(9);
 const json = `{"x":10, "y":20}`;
 const coordinates:{x:number, y:number} = JSON.parse(json);
 console.log('coordinates:', coordinates);
+
+// 2  when we declare a variable on one line and initialize it later
+
+let words = ['one', 'two', 'three'];
+let isTwo:boolean;
+
+for(let i = 0; i < words.length; i++){
+    if(words[i] === 'two'){
+        isTwo=true;
+    }
+}
