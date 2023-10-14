42d30145c3e8ebac1ef5f2a955a9e7f70fc998fa
190101101 <190101101@mail.ru>
2023-10-15 01:22:04 +0400

    05 object for annotation\28 different types

--- src/functions.ts
+++ src/functions.ts
@@ -6,3 +6,10 @@ const subtract = (a:number, b:number):number => {
     return a - b;
 }
 
+function divide(a:number,b:number):number {
+    return a / b;
+}
+
+const multiply = function(a:number,b:number):number {
+    return a * b;
+}
