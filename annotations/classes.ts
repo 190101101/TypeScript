abd0f8aac717a78cff817ff96c561c70c15ad132
190101101 <190101101@mail.ru>
2023-10-18 08:55:20 +0400

    09 func using classes\52 object method modifiers

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,19 +1,26 @@
 class Vehicle{
-    drive():void{
-        console.log('i am driving');
-    }
 
-    stop():void{
+    public stop():void{
         console.log('i am stopped');
+    }
+    
+    protected beep():void{
+        console.log('beeep');
     }
 }
 
 class Car extends Vehicle{
-    drive():void{
+    private drive():void{
         console.log('i am a car and i m driving')
+    }
+    
+    public startDrivingProcess():void{
+        this.drive();
+        this.beep();
     }
 }
 
 const car = new Car();
-car.drive();
+// car.drive();
+car.startDrivingProcess();
 car.stop();
