4d7d932d26b41862baada591be6bb6304367d322
190101101 <190101101@mail.ru>
2023-10-18 06:28:53 +0400

    06 typed array\35 arrays with different types

--- src/arrays.ts
+++ src/arrays.ts
@@ -19,7 +19,16 @@ const color1 = colors.pop();
 // colors.push(true);
 
 //help with map
-
 colors.map((color:string) : string => {
     return color.toUpperCase();
 })
+
+//flexible types
+const importantDates = [new Date, '2023-18-10'];
+const importantDates2 = [new Date];
+const importantDates3:(Date | String | number)[] = [new Date];
+
+importantDates3.push('2023-18-10');
+importantDates3.push(new Date());
+importantDates3.push(5);
+console.log(importantDates3);
