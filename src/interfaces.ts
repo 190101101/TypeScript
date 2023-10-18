3a0aec062fb1171dba6b6377afa505c0b1add4bf
190101101 <190101101@mail.ru>
2023-10-18 07:12:12 +0400

    base\08 interfaces\44 using interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,14 +1,16 @@
+interface Vehicle{
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: "civic",
   year: 2000,
   broken: true,
 };
 
-const printVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`name: ${vehicle.name}`);
   console.log(`year: ${vehicle.year}`);
   console.log(`broken: ${vehicle.broken}`);
