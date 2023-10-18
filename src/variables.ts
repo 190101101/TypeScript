8ec319f0c86f5c45561d7231a9dbcb1342994105
190101101 <190101101@mail.ru>
2023-10-18 05:09:16 +0400

    05 object for annotation\29 void and never

--- src/variables.ts
+++ src/variables.ts
@@ -1,33 +1,33 @@
-const oranges:number = 5;
-let speed:string = 'fast';
-let hasStatus:boolean = true;
+const oranges: number = 5;
+let speed: string = "fast";
+let hasStatus: boolean = true;
 
-let nothing1:null = null;
-let nothing2:undefined = undefined;
+let nothing1: null = null;
+let nothing2: undefined = undefined;
 
 //built-in objects
-let now:Date = new Date();
+let now: Date = new Date();
 
 //array
-let colors:string[] = ['black', 'green', 'yellow', 'blue'];
-let numbers: number[] = [1,2,3,4]
-let results: boolean[] = [true, false, true]
+let colors: string[] = ["black", "green", "yellow", "blue"];
+let numbers: number[] = [1, 2, 3, 4];
+let results: boolean[] = [true, false, true];
 
 //classes
 
-class Car{}
+class Car {}
 
-let car:Car = new Car();
+let car: Car = new Car();
 
 //object literal
-let point:{x:number; y:number} = {
-    x:10,
-    y:20
-}
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
 
 // functions
-const logNumber: (i:number) => void = (i:number) => {
-    console.log(i);
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
 };
 
 logNumber(9);
@@ -36,28 +36,27 @@ logNumber(9);
 // 1 function returns the 'any' type
 
 const json = `{"x":10, "y":20}`;
-const coordinates:{x:number, y:number} = JSON.parse(json);
-console.log('coordinates:', coordinates);
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log("coordinates:", coordinates);
 
 // 2  when we declare a variable on one line and initialize it later
 
-let words = ['one', 'two', 'three'];
-let isTwo:boolean;
+let words = ["one", "two", "three"];
+let isTwo: boolean;
 
-for(let i = 0; i < words.length; i++){
-    if(words[i] === 'two'){
-        isTwo=true;
-    }
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "two") {
+    isTwo = true;
+  }
 }
-
 
 // 3 variables whoes type cant be inferrad
 
 let myNumbers = [-10, -1, 12];
-let positiveNumber:boolean|number = false;
+let positiveNumber: boolean | number = false;
 
-for(let i = 0; i < myNumbers.length; i++){
-    if(myNumbers[i] > 0){
-        positiveNumber = myNumbers[i];
-    }
+for (let i = 0; i < myNumbers.length; i++) {
+  if (myNumbers[i] > 0) {
+    positiveNumber = myNumbers[i];
+  }
 }
