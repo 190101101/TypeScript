524a277b92762832198d6280f2521096cc458924
190101101 <190101101@mail.ru>
2023-10-18 05:24:49 +0400

    06 typed array\33 arrays in ts

---
+++ src/arrays.ts
@@ -0,0 +1,12 @@
+const colors = ['red', 'green', 'yellow'];
+
+const dates = [new Date(), new Date()];
+
+
+const fruitsByColor = [
+    ['tomato'],
+    ['apple'],
+    ['lemon'],
+]
+
+const fruitsByColor2:string[][] = []
