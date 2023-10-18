4e10b5e4a393ee21521a84df8f34c6d60015a0cb
190101101 <190101101@mail.ru>
2023-10-18 07:15:08 +0400

    08 interfaces\45 interface syntax

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,19 +1,24 @@
 interface Vehicle{
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary():string
 }
 
 const oldCivic = {
   name: "civic",
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary():string{
+    return `name: ${this.name}`;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`name: ${vehicle.name}`);
-  console.log(`year: ${vehicle.year}`);
-  console.log(`broken: ${vehicle.broken}`);
+  //* console.log(`name: ${vehicle.name}`);
+  //* console.log(`year: ${vehicle.year}`);
+  //* console.log(`broken: ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
