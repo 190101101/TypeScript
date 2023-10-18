eac81b4195ca0f5cfc93191638d7e98ffec0ea63
190101101 <190101101@mail.ru>
2023-10-18 06:38:39 +0400

    07 tuples\39 tuples in action

---
+++ src/tuples.ts
@@ -0,0 +1,23 @@
+const drink = {
+    color:'brown',
+    cabonated:true,
+    sugar:40
+}
+
+const cola: [string, boolean, number] = ['brown', true, 40];
+
+//err
+// cola[0] = 1234
+
+//true
+// cola[0] = 'asd'
+
+// * type alias
+type Drink = [string, boolean, number];
+const cola2:Drink = ['brown', true, 30]
+const sprite:Drink = ['clear', true, 30]
+const coffe:Drink = ['black', false, 0]
+
+console.log(cola2);
+console.log(sprite);
+console.log(coffe);
