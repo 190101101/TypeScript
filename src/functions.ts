2c4c440f3be3086638b7f10e023b8ebe9776b2ea
190101101 <190101101@mail.ru>
2023-10-18 05:12:41 +0400

    05 object for annotation\30 destructuring

--- src/functions.ts
+++ src/functions.ts
@@ -18,6 +18,18 @@ const logger = (message: string): void => {
   console.log(message);
 };
 
-const throwError = (message: string):never => {
-    throw new Error(message);
-}
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const todayWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({date, weather}: { date: Date; weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todayWeather);
