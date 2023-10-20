5777bd327ee6528bd07c5e2d326b0a16a79a056c
190101101 <190101101@mail.ru>
2023-10-20 09:06:34 +0400

    10 Design Patterns\62 using type definition files

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -8,6 +8,10 @@ class User{
     };
 
     constructor(){
-        
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: faker.address.latitude(),
+            lng: faker.address.longitude(),
+        }
     }
 }
