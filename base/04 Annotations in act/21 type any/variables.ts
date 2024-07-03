fbb75e133c04af151b1b4fea83c51c87eeff9a18
190101101 <190101101@mail.ru>
2024-07-03 19:40:35 +0400

    04 Annotations in act\21 type any

---
+++ base/04 Annotations in act/21 type any/variables.ts
@@ -0,0 +1,24 @@
+// primitive types
+const oranges: number = 5;
+const speed: string = 'fast';
+const hasDog: boolean = true;
+const nothing: null = null;
+const other: undefined = undefined;
+
+// built-in objects
+const now: Date = new Date();
+
+//array
+let colors: string[] = ['red', 'yellow', 'ping'];
+let numbers: number[] = [1, 2, 3, 4, 5];
+let results: boolean[] = [true, false, true];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
