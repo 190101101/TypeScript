7aa4b1c17010365f698273980dc2ddeba8dd9436
190101101 <190101101@mail.ru>
2024-07-03 07:15:38 +0400

    04 Annotations in act\17 variables

---
+++ base/04 Annotations in act/17 variables/variables.ts
@@ -0,0 +1,9 @@
+// primitive types
+const oranges: number = 5;
+const speed: string = 'fast';
+const hasDog: boolean = true;
+const nothing: null = null;
+const other: undefined = undefined;
+
+// object types
+const now: Date = new Date();
