c14b5a4d0c71fb4f7fb394ee0feb84d5801effd9
190101101 <190101101@mail.ru>
2024-07-04 20:21:09 +0400

    06 typed array\34 what are arrays for in ts

---
+++ dev/src/arrays.ts
@@ -0,0 +1,5 @@
+const colors: string[] = ['red', 'green', 'yellow'];
+const dates = [new Date(), new Date()];
+
+const fruitsByColor = [['tomato'], ['apple'], ['lemon']];
+const fruitsByColor2: string[][] = [];
