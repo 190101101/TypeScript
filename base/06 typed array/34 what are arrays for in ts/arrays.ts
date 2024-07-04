ed5e0aec0dac945b9c096f80aec79c8ff25006c8
190101101 <190101101@mail.ru>
2024-07-04 20:25:28 +0400

    06 typed array\34 what are arrays for in ts

--- base/06 typed array/34 what are arrays for in ts/arrays.ts
+++ base/06 typed array/34 what are arrays for in ts/arrays.ts
@@ -3,3 +3,16 @@ const dates = [new Date(), new Date()];
 
 const fruitsByColor = [['tomato'], ['apple'], ['lemon']];
 const fruitsByColor2: string[][] = [];
+
+//? Help with inference when extracting values
+const color = colors[0];
+const color1 = colors.pop();
+
+//? prevent incompatible values
+//! colors.push(true);
+
+//? help with map
+const newColors = colors.map((color: string): string => {
+  return color.toUpperCase();
+});
+console.log(newColors);
