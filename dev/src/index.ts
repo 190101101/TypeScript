2ade52d141fb4ec0ebaa09a3baa9e573798aa374
190101101 <190101101@mail.ru>
2024-07-13 05:26:26 +0400

    LinkedList

--- dev/src/index.ts
+++ dev/src/index.ts
@@ -1,5 +1,6 @@
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { BubbleSort } from './BubbleSort';
 
 const numbersCollection = new NumbersCollection([5, 2, 4, 3, 1]);
@@ -11,3 +12,14 @@ const charactersCollection = new CharactersCollection('zxcasdqwdqw');
 const bubbleSortCharacter = new BubbleSort(charactersCollection);
 bubbleSortCharacter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-11);
+linkedList.add(-3);
+linkedList.add(7);
+linkedList.add(47);
+linkedList.add(5);
+const bubbleSortlinkedList = new BubbleSort(linkedList);
+bubbleSortlinkedList.sort();
+linkedList.print();
