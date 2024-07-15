0d5e9d9070827580941f14beff634f1542b0b33d
190101101 <190101101@mail.ru>
2024-07-15 23:33:19 +0400

    12 reusable code\118 filereader is not finished

---
+++ dev/src/CsvFileReaderBack.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import { dateStringToDate } from './util';
+import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class CsvFileReaderBack {
+  data: MatchData[] = [];
+
+  constructor(public filename: string) {}
+
+  read(): void {
+    const filePath = path.join(__dirname, '..', this.filename);
+    this.data = fs
+      .readFileSync(filePath, {
+        encoding: 'utf8',
+      })
+      .split('\n')
+      .map((row: string): string[] => {
+        return row.split(',');
+      })
+      .map((row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
+        ];
+      });
+  }
+}
