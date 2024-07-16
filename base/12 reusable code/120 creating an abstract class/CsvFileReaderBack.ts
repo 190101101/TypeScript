d8a44f7ae4c1036c189df7103a0f4396ad46080d
190101101 <190101101@mail.ru>
2024-07-16 05:24:31 +0400

    12 reusable code\120 creating an abstract class

--- base/12 reusable code/120 creating an abstract class/CsvFileReaderBack.ts
+++ base/12 reusable code/120 creating an abstract class/CsvFileReaderBack.ts
@@ -1,15 +1,14 @@
 import fs from 'fs';
 import path from 'path';
+import { dateStringToDate } from './util';
 import { MatchResult } from './MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export abstract class CsvFileReader {
+export class CsvFileReaderBack {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): MatchData;
 
   read(): void {
     const filePath = path.join(__dirname, '..', this.filename);
@@ -21,6 +20,16 @@ export abstract class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map(this.mapRow);
+      .map((row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6],
+        ];
+      });
   }
 }
