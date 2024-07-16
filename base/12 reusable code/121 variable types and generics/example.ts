f3160b8d8fc65a79fb1ab7e72b9a10bd385ee6e2
190101101 <190101101@mail.ru>
2024-07-16 05:43:14 +0400

    12 reusable code\121 variable types and generics

---
+++ base/12 reusable code/121 variable types and generics/example.ts
@@ -0,0 +1,44 @@
+const addOne = (a: number): number => {
+  return a + 1;
+};
+
+const addTwo = (a: number): number => {
+  return a + 2;
+};
+
+const addThree = (a: number): number => {
+  return a + 3;
+};
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+//? console.log(add(100, 1));
+
+class HoldNumber {
+  data: number = 0;
+}
+
+class HoldString {
+  data: string = '';
+}
+
+const holdNumber = new HoldNumber();
+holdNumber.data = 111;
+
+const holdString = new HoldString();
+holdString.data = 'zxc';
+
+//* generics
+class HoldAnything<TypeOfData> {
+  data: TypeOfData = {} as TypeOfData;
+}
+
+const holdAnythingNumber = new HoldAnything<number>();
+holdAnythingNumber.data = 111;
+console.log(holdAnythingNumber.data);
+
+const holdAnythingString = new HoldAnything<string>();
+holdAnythingString.data = 'zxc';
+console.log(holdAnythingString.data);
