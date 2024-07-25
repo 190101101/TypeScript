b7c42b16a8725d0bd8038175919db33c75040e2e
190101101 <190101101@mail.ru>
2024-07-25 19:03:11 +0400

    12 reusable code\134 creation of reporter

---
+++ base/12 reusable code/134 creation of reporter/index.ts
@@ -0,0 +1,11 @@
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+
+// Create an object that implements the DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an object of MatchReader and pass DataReader implementation
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+// matchReader.matches
