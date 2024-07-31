5744b8de303b53460312dab7140c7d4f5e5d3aaf
190101101 <190101101@mail.ru>
2024-08-01 03:00:55 +0400

    16 express and ts extended\174 example metadata

--- library/example/metadata.ts
+++ library/example/metadata.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 
+//1
 const box = {
   color: 'black',
 };
@@ -16,3 +17,22 @@ const width = Reflect.getMetadata('width', box);
 Reflect.defineMetadata('note', 'hello', box, 'color');
 const note2 = Reflect.getMetadata('note', box, 'color');
 console.log(note2);
+
+//2
+class Car {
+  color: string = 'black';
+
+  @markFunction('big secret')
+  drive(): void {
+    console.log('i am driving');
+  }
+}
+
+function markFunction(secret: string) {
+  return function (target: Car, key: string) {
+    Reflect.defineMetadata('info', secret, target, key);
+  };
+}
+
+const info = Reflect.getMetadata('info', Car.prototype, 'drive');
+console.log(info);
