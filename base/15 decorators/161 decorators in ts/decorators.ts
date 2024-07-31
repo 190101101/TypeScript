7ae5de01e64eba9079b0dec161b5e11dd2dcd6f0
190101101 <190101101@mail.ru>
2024-07-31 08:53:27 +0400

    15 decorators\162 more about decorators

---
+++ base/15 decorators/161 decorators in ts/decorators.ts
@@ -0,0 +1,17 @@
+class Car {
+  color: string = 'white';
+
+  get formattedColor(): string {
+    return `This car color is ${this.color};`;
+  }
+
+  @testDecorator
+  drive(): void {
+    console.group('I am driving');
+  }
+}
+
+function testDecorator(target: any, key: string): void {
+  console.log('Target', target);
+  console.log('Key', key);
+}
