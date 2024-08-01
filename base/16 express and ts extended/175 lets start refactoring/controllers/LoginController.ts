fd8a05a08b643fa96b52ef3d4b2a63cc0c5ea5fe
190101101 <190101101@mail.ru>
2024-08-01 05:47:09 +0400

    16 express and ts extended\175 start refactoring

---
+++ base/16 express and ts extended/175 lets start refactoring/controllers/LoginController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+
+@controller('/')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response) {
+    res.send(`
+    <form action="/login" method="POST">
+      <div>
+        <label>email</label>
+        <input type="text" name="email" placeholder="email" />
+      </div>
+      <div>
+        <label>password</label>
+        <input type="text" name="password" placeholder="password" />
+      </div>
+      <div>
+        <button type="submit">submit</button>
+      </div>
+    </form>
+  `);
+  });
+}
