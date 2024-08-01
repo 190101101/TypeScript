bda380d47ab6d98359ebd6eb7afb4788cd10ed5a
190101101 <190101101@mail.ru>
2024-08-01 08:30:21 +0400

    16 express and ts extended\181 enum methods

---
+++ base/16 express and ts extended/181 enum methods/controllers/LoginController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { get, controller } from '../decorators';
+
+@controller('/auth')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response) {
+    res.send(`
+    <form action="/login" method="POST">
+      <div>
+        <label>email</label>
+        <input type="text" name="email" placeholder="email" />
+      </div>
+      <div>
+        <label>password</label>
+        <input type="text" name="password" placeholder="password" />
+      </div>
+      <div>
+        <button type="submit">submit</button>
+      </div>
+    </form>
+  `);
+  }
+}
