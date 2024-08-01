031e016c610494c3155b3c04b7efd63ace93cfca
190101101 <190101101@mail.ru>
2024-08-02 03:44:48 +0400

    17 react to ts\196 processing component state

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-interface AddProps{
-  color?:string
+interface AddProps {
+  color?: string;
 }
 
-class App extends React.Component<AddProps>{
-  render(){
-    return <div>{this.props.color}</div>
+class App extends React.Component<AddProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>+</button>
+        <button onClick={this.onDecrement}>-</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
@@ -15,4 +31,4 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(<App/>)
+root.render(<App />);
