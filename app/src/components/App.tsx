ab919af8fad5f259329c20df1ab58073d30a8a9b
190101101 <190101101@mail.ru>
2024-12-15 19:00:50 +0400

    update 15.12.2024

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -8,12 +8,29 @@ interface AppProps {
   fetchTodos(): any;
 }
 
-export class App extends React.Component<AppProps> {
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderTodos = (): JSX.Element[] => {
+    return this.props.todos.map((todo: Todo, index) => {
+      return <div key={index}>{todo.title}</div>;
+    });
+  };
+
   render() {
-    return <div>Hello Pepikus</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>click</button>
+        {this.renderTodos()}
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
-  return { todos: state.todos };
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
 };
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,14 +6,14 @@ import { thunk } from 'redux-thunk';
 import { App } from './components/App';
 import { reducers } from './reducers';
 
-// const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  // <Provider store={store}>
-  <App />
-  // </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
